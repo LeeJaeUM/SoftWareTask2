73fcac29e806df6c3c80def641ad2ace0de852b3
JAeUNg <lsam3333333@gmail.com>
2023-11-30 05:49:07 +0900

    Add Restart Button
    game clear -> title button

--- Assets/02_Scripts/GameEndUI.cs
+++ Assets/02_Scripts/GameEndUI.cs
@@ -6,6 +6,7 @@ public class GameEndUI : MonoBehaviour
 {
     public GameObject uiObject1;
     public GameObject uiObject2;
+    public GameObject reStartButton;
 
     // 다른 곳에서 호출하여 사용할 수 있도록 public 메서드 생성
     public void ToggleUIObjects()
@@ -13,7 +14,7 @@ public class GameEndUI : MonoBehaviour
         // 두 UI 오브젝트를 모두 비활성화
         uiObject1.SetActive(false);
         uiObject2.SetActive(false);
-
+        reStartButton.SetActive(true);
         // activateFirstObject가 true이면 첫 번째 UI 오브젝트를 활성화, 그렇지 않으면 두 번째 UI 오브젝트를 활성화
         if (GameManager.Instance.missCount < 3)
         {
